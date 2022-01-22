f47b3539db7c50df6f0070907570b34f0be6cf2e
erase2004 <erase2004@gmail.com>
2022-01-22 20:25:12 +0800

    1. add user profile page 2. update user profile related link

---
+++ src/components/UserFollowingsCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="card">
+    <div class="card-header">
+      <strong>{{ followerings.length }}</strong> followers (追隨者)
+    </div>
+    <div class="card-body">
+      <router-link
+        v-for="user in followerings"
+        :key="user.id"
+        :to="{ name: 'user', params: {id: user.id}}"
+      >
+        <img
+          :src="user.image"
+          class="avatar"
+          width="60"
+          height="60"
+        >
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    followerings: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
